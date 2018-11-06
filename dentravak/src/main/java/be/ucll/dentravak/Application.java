59bfabd800666cf96c0d70e73f6aea4290e6ec8f
Jonas De Roover <jonasderoover@gmail.com>
2018-11-06 10:36:38 +0100

    application class

---
+++ dentravak/src/main/java/be/ucll/dentravak/Application.java
@@ -0,0 +1,12 @@
+package be.ucll.dentravak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
