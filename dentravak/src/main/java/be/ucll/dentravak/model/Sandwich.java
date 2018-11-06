e805674c449d7bacc683c77d8ca807bf8b351faf
SametHalili <sametalenvers@gmail.com>
2018-11-06 10:28:24 +0100

    init project

---
+++ dentravak/src/main/java/be/ucll/dentravak/model/Sandwich.java
@@ -0,0 +1,37 @@
+package be.ucll.dentravak.model;
+
+public class Sandwich {
+    private String name;
+    private String ingredients;
+    private double price;
+
+    public Sandwich(String name, String ingredients, double price) {
+        this.name = name;
+        this.ingredients = ingredients;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
